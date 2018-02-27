61bc35c282cdd5e18289bc95442ac7f6b68b4f81
Joaquin <jpinilloslr@gmail.com>
2018-02-26 23:13:25 -0300

    Add JumpInstruction.

---
+++ Chip8Emulator/Core/InstructionSet/JumpInstruction.cs
@@ -0,0 +1,14 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // 1nnn - JP addr
+    public class JumpInstruction : IInstruction
+    {
+        public bool Match(ushort opcode) => (opcode & 0xF000) == 0x1000;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            var address = (ushort)(opcode & 0x0FFF);
+            system.Cpu.PC = address;
+        }
+    }
+}
