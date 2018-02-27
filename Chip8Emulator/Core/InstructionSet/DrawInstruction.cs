a89dcf80090303595e0d2f103eb98a0d6c8ac560
Joaquin <jpinilloslr@gmail.com>
2018-02-26 22:04:07 -0300

    Add mock DrawInstruction.

---
+++ Chip8Emulator/Core/InstructionSet/DrawInstruction.cs
@@ -0,0 +1,14 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // Dxyn - DRW Vx, Vy, nibble
+    public class DrawInstruction : IInstruction
+    {
+        public bool Match(ushort opcode) => (opcode & 0xF000) == 0xD000;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            // TODO: Draw
+            system.Cpu.PC += 2;
+        }
+    }
+}
