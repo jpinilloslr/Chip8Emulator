ba5a7769078c0e3bb6ee46a077af0a85d8c6cf7a
Joaquin <jpinilloslr@gmail.com>
2018-02-26 22:21:00 -0300

    Add ReturnInstruction.

---
+++ Chip8Emulator/Core/InstructionSet/ReturnInstruction.cs
@@ -0,0 +1,14 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // 00EE - RET
+    public class ReturnInstruction : IInstruction
+    {
+        public bool Match(ushort opcode) => opcode == 0x00EE;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            var address = system.Stack.Pop();
+            system.Cpu.PC = address;
+        }
+    }
+}
