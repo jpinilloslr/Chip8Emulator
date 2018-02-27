e15d2a81f8484d4214fd1b78bbdd7ca1eb6bf704
Joaquin <jpinilloslr@gmail.com>
2018-02-27 00:59:13 -0300

    Add Fx65 and Fx55 instructions.

---
+++ Chip8Emulator/Core/InstructionSet/OpFx55.cs
@@ -0,0 +1,18 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // Fx55 - LD [I], Vx
+    public class OpFx55 : IInstruction
+    {
+        public bool Match(ushort opcode) => (opcode & 0xF0FF) == 0xF055;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            var registerIndex = (byte)((opcode & 0x0F00) >> 8);
+            for (var i = 0; i < registerIndex; i++)
+            {
+                system.Memory[system.Cpu.I + i] = system.Cpu.V[i];
+            }
+            system.Cpu.PC += 2;
+        }
+    }
+}
---
+++ Chip8Emulator/Core/InstructionSet/OpFx65.cs
@@ -0,0 +1,18 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // Fx65 - LD Vx, [I]
+    public class OpFx65 : IInstruction
+    {
+        public bool Match(ushort opcode) => (opcode & 0xF0FF) == 0xF065;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            var registerIndex = (byte)((opcode & 0x0F00) >> 8);
+            for (var i = 0; i < registerIndex; i++)
+            {
+                system.Cpu.V[i] = system.Memory[system.Cpu.I + i];
+            }
+            system.Cpu.PC += 2;
+        }
+    }
+}
