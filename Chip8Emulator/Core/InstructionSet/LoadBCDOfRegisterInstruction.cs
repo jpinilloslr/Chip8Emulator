5f631d28285b75929e144693f95caabd78e7fa65
Joaquin <jpinilloslr@gmail.com>
2018-02-27 00:32:48 -0300

    Add LoadBCDOfRegisterInstruction.

---
+++ Chip8Emulator/Core/InstructionSet/LoadBCDOfRegisterInstruction.cs
@@ -0,0 +1,17 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // Fx33 - LD B, Vx
+    public class LoadBCDOfRegisterInstruction : IInstruction
+    {
+        public bool Match(ushort opcode) => (opcode & 0xF0FF) == 0xF033;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            var registerIndex = (byte)((opcode & 0x0F00) >> 8);
+            system.Memory[system.Cpu.I] = (byte)(system.Cpu.V[registerIndex] / 100);
+            system.Memory[system.Cpu.I + 1] = (byte)((system.Cpu.V[registerIndex] / 10) % 10);
+            system.Memory[system.Cpu.I + 2] = (byte)((system.Cpu.V[registerIndex] % 100) % 10);
+            system.Cpu.PC += 2;
+        }
+    }
+}
