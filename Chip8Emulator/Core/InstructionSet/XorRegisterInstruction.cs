6958c89f0d661ac51a0394d211e674238b86a1ff
Joaquin <jpinilloslr@gmail.com>
2018-02-27 00:02:30 -0300

    Add bitwise operations instructions.

---
+++ Chip8Emulator/Core/InstructionSet/XorRegisterInstruction.cs
@@ -0,0 +1,16 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // 8xy3 - XOR Vx, Vy
+    public class XorRegisterInstruction : IInstruction
+    {
+        public bool Match(ushort opcode) => (opcode & 0xF00F) == 0x8003;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            var registerIndex1 = (byte)((opcode & 0x0F00) >> 8);
+            var registerIndex2 = (byte)((opcode & 0x00F0) >> 4);
+            system.Cpu.V[registerIndex1] ^= system.Cpu.V[registerIndex2];
+            system.Cpu.PC += 2;
+        }
+    }
+}
