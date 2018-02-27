dab35221c562b6f7ce207f628c41a78c7e76dd83
Joaquin <jpinilloslr@gmail.com>
2018-02-26 22:26:14 -0300

    Add SystemInstruction.

---
+++ Chip8Emulator/Core/InstructionSet/SystemInstruction.cs
@@ -0,0 +1,15 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // 0nnn - SYS addr
+    public class SystemInstruction : IInstruction
+    {
+        public bool Match(ushort opcode) => 
+            (opcode & 0xF000) == 0x0000 && opcode != 0x00EE && opcode != 0x00E0;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            // Obsolete instruction, ignore
+            system.Cpu.PC += 2;
+        }
+    }
+}
