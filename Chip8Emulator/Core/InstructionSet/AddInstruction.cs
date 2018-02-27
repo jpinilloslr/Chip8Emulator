fcae372df899254e8fe7daf6f1e1749c6afa8d3d
Joaquin <jpinilloslr@gmail.com>
2018-02-26 23:54:56 -0300

    Add AddInstruction.

---
+++ Chip8Emulator/Core/InstructionSet/AddInstruction.cs
@@ -0,0 +1,16 @@
+﻿namespace Chip8Emulator.Core.InstructionSet
+{
+    // 7xkk - ADD Vx, byte
+    public class AddInstruction : IInstruction
+    {
+        public bool Match(ushort opcode) => (opcode & 0xF000) == 0x7000;
+
+        public void Run(ushort opcode, Chip8System system)
+        {
+            var registerInedx = (ushort)((opcode & 0x0F00) >> 8);
+            var value = (byte)(opcode & 0x00FF);
+            system.Cpu.V[registerInedx] += value;
+            system.Cpu.PC += 2;
+        }
+    }
+}
